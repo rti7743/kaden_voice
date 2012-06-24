99370e6d35e59120ae045ccc60a83b69fd8da23b
rti <super.rti@gmail.com>
2012-06-24 18:51:28 +0900

    linux のビルド

---
+++ naichichi2/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if "$1" == "clean"; then
+        (cd julius ; make clean ; cd .. );
+        (cd liblinear ; make clean ; cd ..);
+#        (cd mecab ; make clean; cd ..  );
+        (cd lua ; make clean; cd ..  );
+        (cd naichichi2 ; make clean; cd .. );
+else
+        (cd julius ; ./configure --with-alsa ; make ; cd .. );
+        (cd liblinear ; ./configure ; make ; cd .. );
+#        (cd mecab ; ./configure ; make ; cd .. );
+        (cd lua ; ./configure ; make ; cd ..  );
+        (cd naichichi2 ; ./configure ; make ; cd .. );
+
+fi
